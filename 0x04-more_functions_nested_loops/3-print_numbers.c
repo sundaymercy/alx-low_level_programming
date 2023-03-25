c8451a728f9b93b41494d20edf2f08b00b72004e
sundaymercy <taiwomercy090@gmail.com>
2023-03-25 03:22:35 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,29 +1,19 @@
-#include "main.h"
-
-
-
-/**
-
- * _islower - Checks if a character is lowercase.
-
- * @c: The character to be checked.
-
- *
-
- * Return: 1 if character is lowercase, 0 otherwise.
-
+/*
+ * File: 3-print_numbers.c
+ * Auth: Ameh Jacob E
  */
 
-int _islower(int c)
+#include "holberton.h"
 
+/**
+ * print_numbers - Prints the numbers from 0-9.
+ */
+void print_numbers(void)
 {
+	int num;
 
-	if (c >= 'a' && c <= 'z')
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
 
-		return (1);
-
-	else
-
-		return (0);
-
+	_putchar('\n');
 }
