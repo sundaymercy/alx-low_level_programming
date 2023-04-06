7673bd4bdd5a37a42cdb702548f1fdc90fb0b6f4
sundaymercy <taiwomercy090@gmail.com>
2023-04-05 17:07:07 -0700

    She locked away a secret, deep inside herself

--- 0-puts_recursion.c
+++ 0-puts_recursion.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string, followed by a new line.
- * @s: The string to be printed.
+ * _puts_recursion - Prints a string, followed by a new line
+ *
+ * @s:The string to be printed
+ * Return: Always 0.
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-
-	else
-		_putchar('\n');
+if (*s == '\0')
+_putchar('\n');
+else
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
 }
