da759d9ce72d5f772d5a6a8d112cdb1bca0f4bb8
sundaymercy <taiwomercy090@gmail.com>
2023-04-13 14:40:36 -0700

    malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * malloc_checked - allocate a space
+ * @b: unsigned int
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+void *x;
+
+x = malloc(b);
+if (x == NULL)
+{
+	exit(98);
+}
+return (x);
+}
