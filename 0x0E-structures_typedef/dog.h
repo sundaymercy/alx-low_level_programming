42d93d41a8469d76aac2f234e780eca9bf222b12
sundaymercy <taiwomercy090@gmail.com>
2023-04-18 07:04:52 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef MAIN
+#define MAIN
+
+/**
+ * struct dog - Defines a dogs attributes
+ * @name: A string for the dogs name
+ * @age: An integer for the dogs age
+ * @owner: A string for the owners name
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
