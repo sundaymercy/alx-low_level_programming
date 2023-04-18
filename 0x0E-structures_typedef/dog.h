0d8c3145b886ca0c0e6a67f3dce102754c17f003
sundaymercy <taiwomercy090@gmail.com>
2023-04-18 07:28:16 -0700

    free

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,21 +1,18 @@
-#ifndef MAIN
-#define MAIN
+#include <stdio.h>
+#include "dog.h"
 
 /**
- * struct dog - Defines a dogs attributes
- * @name: A string for the dogs name
- * @age: An integer for the dogs age
- * @owner: A string for the owners name
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-typedef struct dog
+int main(void)
 {
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+    dog_t my_dog;
 
-#endif
+    my_dog.name = "bingleo";
+    my_dog.age = 3.5;
+    my_dog.owner = "taiwo";
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+    return (0);
+}
