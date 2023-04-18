d4dcd7a7170c66b30eaf64740089bb1e83485c38
sundaymercy <taiwomercy090@gmail.com>
2023-04-18 04:52:30 -0700

    like macro

--- 0x0D-preprocessor/main.c
+++ 0x0D-preprocessor/main.c
@@ -1,17 +1,6 @@
-#include "0-object_like_macro.h"
-#include "0-object_like_macro.h"
-#include <stdio.h>
+#ifndef MAIN
+#define MAIN
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-int s;
+#define SIZE 1024
 
-s = 98 + SIZE;
-printf("%d\n", s);
-return (0);
-}
+#endif
