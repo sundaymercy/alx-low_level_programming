9f0ad236f4d51ee6a0bb5fef686fc7f54243febb
sundaymercy <taiwomercy090@gmail.com>
2023-05-04 13:22:26 -0700

    main

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,13 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stddef.h>
 
 int _putchar(char c);
-unsigned int binary_to_uint(constant char *b);
-void print_binary(unsign long int n);
-int get_bit(unsign long int n, unsign int index);
-int set_bit(unsigned long int *n, unsign  int index);
-int clear_bit(unsign long int *n, unsign int index);
-unsigned int flip_bits(unsign long int n, unsign long int m);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
-#endif /* MAIN_H */
+
+#endif
+Footer
+©️ 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacies
+Securities
