2aafa3e62f04fc3768d542b274c65cdceff092a4
sundaymercy <taiwomercy090@gmail.com>
2023-05-08 15:46:09 -0700

    all

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -45,13 +45,13 @@ void close_file(int fd)
 }
 
 /**
- * main - Copied all the contents of a file to another file.
+ * main - Copies the content of the file to another file.
  * @argc: The number of arguments supplied to the program.
- * @argv: the array of pointers to the arguments.
+ * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success.
  *
- * Description: the arguments count is incorrect - exit code 97.
+ * Description: If the argument count is incorrect - exit code 97.
  *              If file_from does not exist or cannot be read - exit code 98.
  *              If file_to cannot be created or written to - exit code 99.
  *              If file_to or file_from cannot be closed - exit code 100.
@@ -100,4 +100,4 @@ int main(int argc, char *argv[])
 	close_file(to);
 
 	return (0);
-}}
+}
