8c64609e2e025c400d58fa4e651bf145a421627b
sundaymercy <taiwomercy090@gmail.com>
2023-07-07 05:13:39 -0700

    main

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,14 +1,18 @@
-#include "hash_tables.h"
-
 /**
- * key_index - implementation of the djb2 algorithm
- * @key: string used to generate hash value
- * @size: size of the hash table
- * Return: value of the hash (index of hash table)
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
  */
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
+unsigned long int hash_djb2(const unsigned char *str)
 {
-	if (!key || !size)
-		return (0);
-	return (hash_djb2(key) % size);
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
